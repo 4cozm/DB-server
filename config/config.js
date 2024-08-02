b2d3920c492cfe1662eecdbc4cfd6d4090b31f6f
4cozm <17328rm@gmail.com>
2024-08-02 21:31:59 +0900

    fix : DB에 DATABASE 이름을 포함해서 깊게 접속해서 에러 발생

--- config/config.js
+++ config/config.js
@@ -3,21 +3,18 @@ import * as env from "../constants/env.js";
 export const config = {
   SHARDS: {
     1: {
-      name: env.SHARD_1_NAME,
       user: env.SHARD_1_USER,
       password: env.SHARD_1_PASSWORD,
       host: env.SHARD_1_HOST,
       port: env.SHARD_1_PORT,
     },
     2: {
-      name: env.SHARD_2_NAME,
       user: env.SHARD_2_USER,
       password: env.SHARD_2_PASSWORD,
       host: env.SHARD_2_HOST,
       port: env.SHARD_2_PORT,
     },
     3: {
-      name: env.SHARD_3_NAME,
       user: env.SHARD_3_USER,
       password: env.SHARD_3_PASSWORD,
       host: env.SHARD_3_HOST,
