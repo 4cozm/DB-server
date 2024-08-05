90f17f277ad4b213e7b4a04185290aa560774e98
4cozm <17328rm@gmail.com>
2024-08-05 10:49:52 +0900

    fix: api 예외처리 추가

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,10 +1,13 @@
 import { DbConnections } from "../utils/connect.js";
 import { resetAllData } from "../utils/migration/createSchema.js";
 
-
-export const migration = async () => {
-  for (const shard of Object.values(DbConnections)) {
-    await resetAllData(shard);
+export const migration = async (req, res) => {
+  try {
+    for (const shard of Object.values(DbConnections)) {
+      await resetAllData(shard);
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: "마이그레이션 중 오류 발생" });
   }
 };
 
@@ -13,11 +16,10 @@ export const getTables = async () => {
     const connection = await mysql.createConnection(shard);
     const getTables = `SHOW DATABASES;`;
 
-    try{
-
-    }catch(error){
-      console.error("테이블을 불러오는 중 오류 발생",error);
-    }finally{
+    try {
+    } catch (error) {
+      console.error("테이블을 불러오는 중 오류 발생", error);
+    } finally {
       await connection.end();
     }
   }
