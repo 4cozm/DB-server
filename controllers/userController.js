d126d844ca44edb18668573dc92455fbdcc32282
4cozm <17328rm@gmail.com>
2024-08-16 19:31:06 +0900

    fix: 유저 중복 닉네임 검사시 에러가 발생하는 문제

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,10 @@ import { getShardByKey, getShardNumber, saveShard } from "../db/shardUtils.js";
 import { toCamelCase } from "../utils/transformCase.js";
 import fatalError from "../error/fatalError.js";
 import formatDate from "../utils/dateFormatter.js";
+import { mainDbConnections } from "../db/connect.js";
 
 const SQL_QUERIES = {
-  FIND_USER_BY_PLAYER_ID: "SELECT * FROM account WHERE player_id = ?",
+  FIND_USER_BY_PLAYER_ID: "SELECT * FROM Shards WHERE `key` = ? AND `table` = 'account'",
   CREATE_USER: "INSERT INTO account (player_id, pw, name) VALUES (?, ?, ?)",
   UPDATE_USER_LOGIN: "UPDATE account SET last_login = CURRENT_TIMESTAMP WHERE player_id = ?",
   FIND_MONEY_BY_PLAYER_ID: "SELECT money FROM money WHERE player_id = ?",
@@ -85,14 +86,10 @@ export const findMoneyByPlayerId = async (req, res) => {
 };
 
 const accountDuplicateCheck = async (player_id) => {
-  try {
-    const shard = await getShardByKey(player_id, "USER_DB", "account");
-    const [rows] = await shard.query(SQL_QUERIES.FIND_USER_BY_PLAYER_ID, [player_id]);
-    if (rows.length > 0) {
-      return false;
-    }
-    return true;
-  } catch (error) {
-    console.error(error);
+  const shard = await mainDbConnections();
+  const [rows] = await shard.query(SQL_QUERIES.FIND_USER_BY_PLAYER_ID, [player_id]);
+  if (rows.length > 0) {
+    return false;
   }
+  return true;
 };
