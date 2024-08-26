69e4288cddda26861000045ef8de0402dcf417af
4cozm <17328rm@gmail.com>
2024-08-26 22:29:57 +0900

    update:elastic cache flush 명령 추가

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,6 +1,6 @@
-import { DbConnections, makeShardsConnect, shardConnections } from "../db/connect.js";
-import { getQueries, resetAllData } from "../utils/migration/createSchema.js";
-import { format } from "sql-formatter";
+import { DbConnections, makeShardsConnect, shardConnections } from '../db/connect.js';
+import { getQueries, resetAllData } from '../utils/migration/createSchema.js';
+import { format } from 'sql-formatter';
 
 export const resetAllSchema = async (req, res) => {
   try {
@@ -10,7 +10,7 @@ export const resetAllSchema = async (req, res) => {
       await resetAllData(shard);
     }
     const executed = await getQueries();
-    const htmlFormattedQuery = format(executed).replace(/\n/g, "<br>");
+    const htmlFormattedQuery = format(executed).replace(/\n/g, '<br>');
     res.status(201).send(htmlFormattedQuery); //JSON으로 보내면 깨져서 html로 변환후 반환
   } catch (error) {
     console.error(error);
@@ -25,9 +25,19 @@ export const getTables = async () => {
 
     try {
     } catch (error) {
-      console.error("테이블을 불러오는 중 오류 발생", error);
+      console.error('테이블을 불러오는 중 오류 발생', error);
     } finally {
       await connection.end();
     }
+  }
+};
+
+export const flushAllData = async (req, res) => {
+  try {
+    await redisClient.flushAll();
+    res.status(200).json({ message: 'done' });
+  } catch (error) {
+    console.error('Redis flushAll에서 오류 발생:', error);
+    res.status(500).json({ errorMessage: 'Redis flushAll에서 오류 발생' });
   }
 };
